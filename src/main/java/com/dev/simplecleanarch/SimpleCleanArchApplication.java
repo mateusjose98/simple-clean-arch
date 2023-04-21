c1ebbe0abe6ac9f0dc334ce74d275410f075dadc
josemateus <josemateus.ufma@gmail.com>
2023-04-20 21:03:58 -0300

    chore: main libs added

---
+++ src/main/java/com/dev/simplecleanarch/SimpleCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.dev.simplecleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCleanArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCleanArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/simplecleanarch/SimpleCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.simplecleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCleanArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
